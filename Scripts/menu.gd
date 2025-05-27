4e45b50108451cc31bb66584ec1f4e9ac3b231b1
jugurta.abdessmed <jugurta.abdessmed@icloud.com>
2025-05-27 17:54:09 +0200

    menu designs 0/3

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -2,14 +2,33 @@ extends CanvasLayer
 
 func _ready():
 	$AnimatedSprite2D.play("default") 
+	$Control.visible = false
 
 func _on_quitter_pressed() -> void:
 	$AnimatedSprite2D.play("default") 
 	self.visible = false
 
 func _on_info_pressed() -> void:
-	$AnimatedSprite2D.play("info")
+	$AnimatedSprite2D.play("kk")
 
 
 func _on_microships_pressed() -> void:
-	$AnimatedSprite2D.play("microships")
+	$AnimatedSprite2D.play("info")
+	$VBoxContainer.visible = false
+	$Control.visible = true
+	$Control/pink.visible = true
+	$Control/yellow.visible = true
+
+
+func _on_return_pressed() -> void:
+	$Control.visible = false
+	self.visible = false
+	$VBoxContainer.visible = true
+	$AnimatedSprite2D.play("default") 
+
+
+func _on_pink_pressed() -> void:
+	$AnimatedSprite2D.play("microship_pink") 
+	$VBoxContainer.visible = false
+	$Control/pink.visible = false
+	$Control/yellow.visible = false
