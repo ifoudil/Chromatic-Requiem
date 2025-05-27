4b077b27127d2e2ee3cf379a5dccaf65e0c9ce44
jugurta.abdessmed <jugurta.abdessmed@icloud.com>
2025-05-27 15:02:55 +0200

    add menu scripts

--- Scripts/pause.gd
+++ Scripts/pause.gd
@@ -4,14 +4,24 @@ var menu = null
 
 func _ready():
 	menu = $TextureRect
-	remove_child(menu)  # On retire le menu au lancement, il n’existe plus dans la scène
+	remove_child(menu)  # On retire le menu au lancement, il n'existe plus dans la scène
 
 func _on_pause_pressed() -> void:
 	if menu.get_parent() == null:
+		# Ouvrir le menu pause
 		add_child(menu)  # On ajoute le menu à CanvasLayer → il apparaît + interactif
+		
+		# FREEZER TOUT LE JEU
+		get_tree().paused = true
+		
 	else:
-		remove_child(menu)  # On enlève le menu → il disparaît + plus d’interaction
-
+		# Fermer le menu pause
+		remove_child(menu)  # On enlève le menu → il disparaît + plus d'interaction
+		
+		# REPRENDRE LE JEU
+		get_tree().paused = false
 
 func _on_button_pressed() -> void:
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/player.tscn")
+	
