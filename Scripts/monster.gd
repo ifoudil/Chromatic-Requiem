dc732f15c94804d5849c6149bb5d0ff71c60d0fa
Islem <islem.foudil@orange.com>
2025-07-28 16:12:43 +0200

    Try reactivity between projectile and the monster - Doesn't work

---
+++ Scripts/monster.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+const SPEED = 100 
+
+@onready var anim_sprite = $AnimatedSprite2D
+@onready var hitbox = $Hitbox
+
+func _ready():
+	anim_sprite.scale = Vector2(3, 3)
+	anim_sprite.play("monster_run")
+	hitbox.area_entered.connect(_on_hitbox_area_entered)
+
+func _physics_process(delta):
+	velocity.x = -SPEED
+	move_and_slide()
+
+func _on_hitbox_area_entered(area):
+	if area.name == "Projectile":
+		queue_free()
