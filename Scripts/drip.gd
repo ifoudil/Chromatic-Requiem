3fe02780e5033673e546fa9c90cd23d52c305c73
jugurta.abdessmed <jugurta.abdessmed@icloud.com>
2025-08-12 17:25:24 +0200

    ajust drip design

--- Scripts/drip.gd
+++ Scripts/drip.gd
@@ -4,12 +4,19 @@ extends Node2D
 @onready var sprite = $AnimatedSprite2D
 
 func _ready():
-	while true:
 		sprite.play("default")
-		await get_tree().create_timer(5).timeout
+		await get_tree().create_timer(3).timeout
 		
 		sprite.play("drip")
-		await get_tree().create_timer(1.322).timeout
+		await get_tree().create_timer(1.1366).timeout
 		
 		sprite.play("default")
-		await get_tree().create_timer(5).timeout
+		await get_tree().create_timer(2).timeout
+		
+		sprite.play("continue")
+		await get_tree().create_timer(0.75).timeout
+		
+		sprite.play("default2")
+	
+		
+		
