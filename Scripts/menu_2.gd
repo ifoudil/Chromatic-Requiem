49f86339c1770db517f35ba18fc88264fb616b19
jugurta.abdessmed <jugurta.abdessmed@icloud.com>
2025-08-16 01:21:53 +0200

    udapte menu animations

--- Scripts/menu_2.gd
+++ Scripts/menu_2.gd
@@ -1,8 +1,10 @@
 extends CanvasLayer
 
+
 var is_animating = false
 
 func _ready():
+	$ColorRect.modulate = Color(1, 1, 1, 0.3)
 	$AnimatedSprite2D.play("default") 
 	$map.disabled = false
 	$Quitter.disabled = false
@@ -33,6 +35,18 @@ func _on_lvl_1_pressed() -> void:
 	await get_tree().create_timer(0.39).timeout
 	$AnimatedSprite2D.play("lvl1up") 
 	await get_tree().create_timer(0.59).timeout
+	
+	$AnimatedSprite2D.modulate = Color(1, 1, 1, 1)
+	
+	var tween1 = create_tween()
+	var tween2 = create_tween()
+	
+	tween1.tween_property($AnimatedSprite2D, "modulate", Color(0, 0, 0, 1), 1.0)
+	tween2.tween_property($ColorRect, "modulate", Color(0, 0, 0, 1), 1.0)
+	
+	
+	await get_tree().create_timer(1.0).timeout
+	
 	self.visible = false
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/lvl1.tscn")
@@ -58,5 +72,24 @@ func _on_lvl_2_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/lvl2.tscn")
 
+
+
+func _on_lvl_2_mouse_entered() -> void:
+	if is_animating:
+		return
+	
+	await get_tree().create_timer(0.1).timeout
+	if not is_animating:
+		$AnimatedSprite2D.play("lvl2") 
+		
+func _on_lvl_2_mouse_exited() -> void:
+	if is_animating:
+		return
+		
+	await get_tree().create_timer(0.1).timeout
+	if not is_animating:
+		$AnimatedSprite2D.play("map")
+	
+	
 func _on_arcade_pressed() -> void:
 	pass
