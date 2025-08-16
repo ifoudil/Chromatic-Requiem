e8956c79187659a89941e688678fc82766ea9b86
jugurta.abdessmed <jugurta.abdessmed@icloud.com>
2025-08-17 00:21:21 +0200

    fixing bog menu and add design final menu 1/3

--- Scripts/menu_2.gd
+++ Scripts/menu_2.gd
@@ -1,55 +1,57 @@
 extends CanvasLayer
 
-
 var is_animating = false
-var no = false
 
 func _ready():
-	no = false
 	$ColorRect.modulate = Color(1, 1, 1, 0.3)
 	$AnimatedSprite2D.play("default") 
-	$map.disabled = false
-	$Quitter.disabled = false
-	$arcade.disabled = false
-	$lvl1.disabled = true
-	$lvl2.disabled = true
+	$map.visible = true
+	$Quitter.visible = true
+	$arcade.visible = true
+	# Niveaux cachés
+	$lvl1.visible = false
+	$lvl2.visible = false
 	get_tree().paused = true
 
 func _on_quitter_pressed() -> void:
 	$AnimatedSprite2D.play("default") 
-	$map.disabled = false
-	$Quitter.disabled = false
-	$arcade.disabled = false
+	$map.visible = true
+	$Quitter.visible = true
+	$arcade.visible = true
+	$lvl1.visible = false
+	$lvl2.visible = false
 	self.visible = false
 	get_tree().paused = false
 
+
+func _on_quitter_mouse_entered() -> void:
+	if  $arcade.visible:
+		$AnimatedSprite2D.play("holdquit") 
+	if  $lvl2.visible:
+		$AnimatedSprite2D.play("holdquit2") 
+
+func _on_quitter_mouse_exited() -> void:
+	if  $lvl2.visible:
+		$AnimatedSprite2D.play("map") 
+	else :
+		$AnimatedSprite2D.play("default") 
+
 func _on_map_pressed() -> void:
 	$AnimatedSprite2D.play("map") 
-	$map.disabled = true
-	$arcade.disabled = true
-	$lvl1.disabled = false
-	$lvl2.disabled = false
-	var no = true
+	$map.visible = false
+	$arcade.visible = false
+	$lvl1.visible = true
+	$lvl2.visible = true
 
 func _on_map_mouse_entered() -> void:
-	if no:
-		
+	if not $map.visible:
 		return
-		
-	if not no:
-		$AnimatedSprite2D.play("holdadv")
-
+	$AnimatedSprite2D.play("holdadv")
 
 func _on_map_mouse_exited() -> void:
-	if no:
+	if not $map.visible:
 		return
-		
-	await get_tree().create_timer(0.05).timeout
-	if not no:
-		$AnimatedSprite2D.play("default")
-
-
-
+	$AnimatedSprite2D.play("default")
 
 func _on_lvl_1_pressed() -> void:
 	is_animating = true
@@ -67,7 +69,6 @@ func _on_lvl_1_pressed() -> void:
 	tween1.tween_property($AnimatedSprite2D, "modulate", Color(0, 0, 0, 1), 1.0)
 	tween2.tween_property($ColorRect, "modulate", Color(0, 0, 0, 1), 1.0)
 	
-	
 	await get_tree().create_timer(1.0).timeout
 	
 	self.visible = false
@@ -75,24 +76,17 @@ func _on_lvl_1_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/lvl1.tscn")
 
 func _on_lvl_1_mouse_entered() -> void:
-	if is_animating:
+	if not $lvl1.visible:
 		return
-	
-	await get_tree().create_timer(0.1).timeout
-	if not is_animating:
-		$AnimatedSprite2D.play("lvl1") 
+	$AnimatedSprite2D.play("lvl1") 
 
 func _on_lvl_1_mouse_exited() -> void:
-	if is_animating:
+	if not $lvl1.visible:
 		return
-		
-	await get_tree().create_timer(0.1).timeout
-	if not is_animating:
-		$AnimatedSprite2D.play("map") 
+	$AnimatedSprite2D.play("map") 
 	
 func _on_lvl_2_pressed() -> void:
 	is_animating = true
-	
 	
 	$AnimatedSprite2D.play("lvl2down") 
 	await get_tree().create_timer(0.39).timeout
@@ -107,42 +101,33 @@ func _on_lvl_2_pressed() -> void:
 	tween1.tween_property($AnimatedSprite2D, "modulate", Color(0, 0, 0, 1), 1.0)
 	tween2.tween_property($ColorRect, "modulate", Color(0, 0, 0, 1), 1.0)
 	
-	
 	await get_tree().create_timer(1.0).timeout
 	
 	self.visible = false
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/lvl2.tscn")
 
-
-
-
 func _on_lvl_2_mouse_entered() -> void:
-	if is_animating:
+	if not $lvl2.visible:
 		return
-	
-	await get_tree().create_timer(0.1).timeout
-	if not is_animating:
-		$AnimatedSprite2D.play("lvl2") 
+	$AnimatedSprite2D.play("lvl2") 
 		
 func _on_lvl_2_mouse_exited() -> void:
-	if is_animating:
+	if not $lvl2.visible:
 		return
-		
-	await get_tree().create_timer(0.1).timeout
-	if not is_animating:
-		$AnimatedSprite2D.play("map")
-	
+	$AnimatedSprite2D.play("map")
 	
 func _on_arcade_pressed() -> void:
 	pass
 
-
 func _on_arcade_mouse_entered() -> void:
-	await get_tree().create_timer(0.05).timeout
+	if not $arcade.visible:
+		return
 	$AnimatedSprite2D.play("holdarcade")
 
-
 func _on_arcade_mouse_exited() -> void:
-	await get_tree().create_timer(0.05).timeout
+	if not $arcade.visible:
+		return
 	$AnimatedSprite2D.play("default")
+
+ 
