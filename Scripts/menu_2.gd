9ba9791a625a5737921b0ef428246eb41a23a78f
jugurta.abdessmed <jugurta.abdessmed@icloud.com>
2025-08-16 20:09:17 +0200

    udapte menu

--- Scripts/menu_2.gd
+++ Scripts/menu_2.gd
@@ -68,9 +68,29 @@ func _on_lvl_1_mouse_exited() -> void:
 		$AnimatedSprite2D.play("map") 
 	
 func _on_lvl_2_pressed() -> void:
+	is_animating = true
+	
+	
+	$AnimatedSprite2D.play("lvl2down") 
+	await get_tree().create_timer(0.39).timeout
+	$AnimatedSprite2D.play("lvl2up") 
+	await get_tree().create_timer(0.59).timeout
+	
+	$AnimatedSprite2D.modulate = Color(1, 1, 1, 1)
+	
+	var tween1 = create_tween()
+	var tween2 = create_tween()
+	
+	tween1.tween_property($AnimatedSprite2D, "modulate", Color(0, 0, 0, 1), 1.0)
+	tween2.tween_property($ColorRect, "modulate", Color(0, 0, 0, 1), 1.0)
+	
+	
+	await get_tree().create_timer(1.0).timeout
+	
 	self.visible = false
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/lvl2.tscn")
+
 
 
 
