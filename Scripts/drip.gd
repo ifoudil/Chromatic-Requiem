3264acddf0a665a6ae1c19c1f4c90c66aa327c95
jugurta.abdessmed <jugurta.abdessmed@icloud.com>
2025-09-05 12:58:15 +0200

    drip modif

--- Scripts/drip.gd
+++ Scripts/drip.gd
@@ -23,11 +23,11 @@ func _ready():
 	sprite.play("default2")
 	await get_tree().create_timer(0.5).timeout
 	sprite.play("flash")
-	await get_tree().create_timer(0.6999).timeout
+	await get_tree().create_timer(0.60).timeout
 	sprite.play("flash2")
 	$continuer.visible = true
 	await get_tree().create_timer(1.99).timeout
-	sprite.play("default2")
+	sprite.play("default3")
 
 func _on_continuer_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/player.tscn")
